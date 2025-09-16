e1d1f5996df4a1ace9740004085bbb126ae451d2
James <1997Jamesjjohnson@gmail.com>
2025-09-16 14:46:08 -0400

    did my day 9 on monday.com

--- projects/broke_n_beauty_ecommerce/frontend/src/App.jsx
+++ projects/broke_n_beauty_ecommerce/frontend/src/App.jsx
@@ -1,16 +1,8 @@
 import { Link, Route, Routes } from 'react-router-dom'
+import LandingPage from './LandingPage'
 
 function Home() {
-  return (
-    <div className="max-w-3xl mx-auto p-6">
-      <h1 className="text-3xl font-bold">Clover Line</h1>
-      <p className="mt-2 text-gray-600">Secure E‑Commerce + SmartFit Size Recommender</p>
-      <div className="mt-6 flex gap-3">
-        <a className="px-4 py-2 rounded-md border" href="http://127.0.0.1:8000/docs" target="_blank" rel="noreferrer">API Docs</a>
-        <Link className="px-4 py-2 rounded-md border" to="/about">About</Link>
-      </div>
-    </div>
-  )
+  return <LandingPage />
 }
 
 function About() {
